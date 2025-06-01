864c4bb4e3444562bffef2a5add9af8fb35c83c5
DSRPT-projects <Aealrisheq@gmail.com>
2025-06-01 15:39:18 -0400

    allowed username login – still not functional, added user id requirement to sign-up, also not complete

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,6 +1,9 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-
+import {
+      initializeAuth,
+      getReactNativePersistence,
+    } from 'firebase/auth';
+    import AsyncStorage from '@react-native-async-storage/async-storage';
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: 'AIzaSyBZ0KE8NLwaykEKXUCjl4QU2KjRyQ6W9Ks',
@@ -11,10 +14,8 @@ const firebaseConfig = {
     appId: '1:282344050418:web:5426836d0e68df868f8a6b'
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase Authentication and get a reference to the service
-const auth = getAuth(app);
-
-export { auth, app }; 
+export const app = initializeApp(firebaseConfig);
+export const auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(AsyncStorage),
+});
